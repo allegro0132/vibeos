597f45db7644d848629873b14ef99e8b1cf1366b
allegro0132 <34514398+allegro0132@users.noreply.github.com>
2026-08-11 10:27:01 +0800

    Remove unused test and traffic modules, and delete CI script
    - Deleted the `test.rs` file which contained test cases for packet roundtrip functionality.
    - Removed the `traffic.rs` file that handled traffic input and output, including state management for SSH packets.
    - Eliminated the `ci.sh` script used for continuous integration, which included various build and test commands.

--- vendor/sunset/src/sunsetlog.rs
+++
@@ -1,9 +0,0 @@
-#[cfg(test)]
-use simplelog::{self, LevelFilter, TestLogger};
-
-#[cfg(test)]
-pub fn init_test_log() {
-    let conf =
-        simplelog::ConfigBuilder::new().add_filter_ignore_str("serde").build();
-    let _ = TestLogger::init(LevelFilter::Trace, conf);
-}
